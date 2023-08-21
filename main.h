83358eae1fbaa57657dd78a60a22714217827ea7
Abdallah988 <abdallat43@icloud.com>
2023-08-22 01:14:41 +0300

    adding headers of handle_binary.c on main.h

--- main.h
+++ main.h
@@ -15,10 +15,6 @@ typedef struct
 	int (*handler)();
 } FormatSpecifier;
 
-int handle_binary(va_list a);
-void print_binary(unsigned int nu, int *size);
-int handle_octal(va_list list);
-void print_octal(unsigned int nu, int *size);
 int _printf(const char *format, ...);
 int handle_char(va_list args);
 int handle_string(va_list args);
@@ -26,4 +22,6 @@ int handle_int(va_list args);
 void print_num(int digit, int *size);
 int handle_percent(va_list args);
 int _putchar(char c);
+void print_bin(unsigned int bi, int *size);
+int handle_binary(va_list bin);
 #endif
