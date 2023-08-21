857053eb4b11bd6269e6942484cb07cb85415702
Abdallah988 <abdallat43@icloud.com>
2023-08-22 00:49:07 +0300

    main.h

--- main.h
+++ main.h
@@ -23,6 +23,7 @@ int _printf(const char *format, ...);
 int handle_char(va_list args);
 int handle_string(va_list args);
 int handle_int(va_list args);
+void print_num(int digit, int *size);
 int handle_percent(va_list args);
 int _putchar(char c);
 #endif
