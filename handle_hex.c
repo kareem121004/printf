6c6f4e1a39ef2c2a1ee61c23629aafff68453930
Abdallah988 <abdallat43@icloud.com>
2023-08-22 03:08:14 +0300

    handle_hex.c

---
+++ handle_hex.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/**
+* print_hex - from decimal to hexadecimal in lowercase
+* @hexa: number
+* @size: size
+* Return: void
+*/
+
+void print_hex(unsigned int hexa, int *size)
+{
+	if (hexa / 16)
+	{
+		print_hex(hexa / 16, size);
+	}
+	*size += 1;
+	if (hexa % 16 < 10)
+		_putchar('0' + hexa % 16);
+	else
+		_putchar('a' + ((hexa % 16) - 10));
+}
+
+/**
+* handle_hex - prints number in lowercase hexadecimal
+* @hex: number
+* Return: the hexadecimal number
+*/
+
+int handle_hex(va_list hex)
+{
+	int he = 0;
+
+	print_hex(va_arg(hex, unsigned int), &he);
+	return (he);
+}
+
+/**
+* print_HEX - from number to hexadecimal in lowercase
+* @HEXA: number
+* @size: size
+* Return: void
+*/
+
+void print_HEX(unsigned int HEXA, int *size)
+{
+	if (HEXA / 16)
+	{
+		print_HEX(HEXA / 16, size);
+	}
+	*size += 1;
+	if (HEXA % 16 < 10)
+		_putchar('0' + HEXA % 16);
+	else
+		_putchar('A' + ((HEXA % 16) - 10));
+}
+
+/**
+* handle_HEX - print numbers in hexadecimal
+* @HEX: number
+* Return: the hexadecimal number
+*/
+
+int handle_HEX(va_list HEX)
+{
+	int hE = 0;
+
+	print_hex(va_arg(HEX, unsigned int), &hE);
+	return (hE);
+}
