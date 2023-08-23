32ebc6f4397ca1e61e5d5b1d6153a981ce78574a
Abdallah988 <abdallat43@icloud.com>
2023-08-23 03:43:32 +0300

    a

--- _printf.c
+++ _printf.c
@@ -52,8 +52,8 @@ int handle(const char *format, va_list args, FormatSpecifier *format_specifier)
 }
 
 /**
- * _printf - print all
- * @format: string.
+ * _printf - print all functions
+ * @format: format string.
  *
  * Return: num or -1
 */
